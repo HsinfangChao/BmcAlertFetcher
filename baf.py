0b3e63e45353cd29be1a629d002fcba5eefd340a
chaohsinfang <chaohsinfang@163.com>
2025-08-12 16:26:06 +0800

    style: Format code for consistency and readability

--- baf.py
+++ baf.py
@@ -3,15 +3,15 @@ import time
 import yaml
 import requests
 from seleniumbase import SB
-from util.utils import setup_logger,decrypt_password,notify_feishu_bot
+from util.utils import setup_logger, decrypt_password, notify_feishu_bot
 
-logger = setup_logger('/var/log/bmc_alert_fetcher.log', 1024 * 1024 * 1024, 5)
+logger = setup_logger("/var/log/bmc_alert_fetcher.log", 1024 * 1024 * 1024, 5)
 current_dir = os.path.dirname(os.path.abspath(__file__))
-config_dir = os.path.join(current_dir, 'config')
-yml_path = os.path.join(config_dir, 'bmc_info.yml')
+config_dir = os.path.join(current_dir, "config")
+yml_path = os.path.join(config_dir, "bmc_info.yml")
 
 
-def get_alert_info(bmc_ip,user_name,password):
+def get_alert_info(bmc_ip, user_name, password):
     with SB() as sb:
         login_url = f"http://{bmc_ip}/#login"
         logs_url = f"http://{bmc_ip}/#logs/alertLog"
@@ -19,16 +19,18 @@ def get_alert_info(bmc_ip,user_name,password):
         sb.js_click_if_present("#details-button")
         sb.js_click_if_present("#proceed-link")
         sb.wait_for_element_present("#userid")
-        sb.type("#userid", user_name)  
-        sb.type("#password", password) 
+        sb.type("#userid", user_name)
+        sb.type("#password", password)
         sb.click("#btn-login")
         sb.wait_for_element_present("#download")
-        seleniumbase_cookies=sb.get_cookies()
+        seleniumbase_cookies = sb.get_cookies()
         session = requests.Session()
         for cookie in seleniumbase_cookies:
-            session.cookies.set(cookie['name'], cookie['value'], domain=cookie.get('domain'))
-        current_alert_url = f"http://{bmc_ip}/#logs/alertLog"  
-        response = session.get(current_alert_url,verify=False)
+            session.cookies.set(
+                cookie["name"], cookie["value"], domain=cookie.get("domain")
+            )
+        current_alert_url = f"http://{bmc_ip}/#logs/alertLog"
+        response = session.get(current_alert_url, verify=False)
         if response.status_code != 200:
             logger.info(f"response content() {response.content.decode('utf-8')}")
             logger.info(f"response status_code {response.status_code}")
@@ -39,31 +41,35 @@ def get_alert_info(bmc_ip,user_name,password):
         logger.info(f"BMC Alert Logs {alert_info}")
         return alert_info
 
+
 def main():
-    
+
     while True:
         try:
-            with open(yml_path, 'r') as file:
+            with open(yml_path, "r") as file:
                 config_info = yaml.safe_load(file)
-                bmc_ips = config_info["bmc_credentials"]['bmc_ips']
-                user_name = config_info["bmc_credentials"]['user_name']
-                key = config_info["bmc_credentials"]['key']
+                bmc_ips = config_info["bmc_credentials"]["bmc_ips"]
+                user_name = config_info["bmc_credentials"]["user_name"]
+                key = config_info["bmc_credentials"]["key"]
                 logger.info(f"key: {key}")
-                encrypted_password = config_info["bmc_credentials"]['encrypted_password']
-                password = decrypt_password(key.encode(),encrypted_password.encode())
+                encrypted_password = config_info["bmc_credentials"][
+                    "encrypted_password"
+                ]
+                password = decrypt_password(key.encode(), encrypted_password.encode())
                 for bmc_ip in bmc_ips:
-                    alert_info=get_alert_info(bmc_ip,user_name,password)
+                    alert_info = get_alert_info(bmc_ip, user_name, password)
                     if "no current alarms" in alert_info:
                         logger.info(f"There is no current alarms")
                     else:
-                        msg =f"bmc_ip:{bmc_ip} alert_info:{alert_info}"
+                        msg = f"bmc_ip:{bmc_ip} alert_info:{alert_info}"
                         notify_feishu_bot(msg)
-                    
+
         except Exception as e:
             logger.error(f"get_alert_info error: {e}")
-        # time.sleep(1)            
+            notify_feishu_bot(f"get_alert_info error: {e}")
+        # time.sleep(1)
         time.sleep(300)
-        
-        
+
+
 if __name__ == "__main__":
     main()
--- util/utils.py
+++ util/utils.py
@@ -6,51 +6,54 @@ from cryptography.fernet import Fernet
 from logging.handlers import RotatingFileHandler
 
 
-
-
 def setup_logger(log_file, max_log_size, backup_count):
     logging.basicConfig(
-        filename=log_file,  
-        level=logging.INFO,         
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        filename=log_file,
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
 
-    logger = logging.getLogger('bmc_alert_fetcher_logger')
-    
-    handler = RotatingFileHandler(log_file, maxBytes=max_log_size, backupCount=backup_count)
+    logger = logging.getLogger("bmc_alert_fetcher_logger")
+
+    handler = RotatingFileHandler(
+        log_file, maxBytes=max_log_size, backupCount=backup_count
+    )
     handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     handler.setFormatter(formatter)
-    
+
     logger.addHandler(handler)
     logger.setLevel(logging.INFO)
 
     return logger
 
 
-logger = setup_logger('/var/log/bmc_alert_fetcher.log', 1024 * 1024 * 1024, 5)
+logger = setup_logger("/var/log/bmc_alert_fetcher.log", 1024 * 1024 * 1024, 5)
 
-def decrypt_password(key,encrypted_password):
+
+def decrypt_password(key, encrypted_password):
     """
     Decrypts an encrypted message using the provided key.
     """
     f = Fernet(key)
-    decrypted_password= f.decrypt(encrypted_password).decode() # Decode from bytes to string
+    decrypted_password = f.decrypt(
+        encrypted_password
+    ).decode()  # Decode from bytes to string
     return decrypted_password
+
 
 def notify_feishu_bot(message):
     config_info = yaml.safe_load(file)
-    webhook_url= config_info["bmc_credentials"]['bmc_ips']
-    payload = {
-        "msg_type": "text",
-        "content": {
-            "text": message
-        }
-    }
-    headers = {'Content-Type': 'application/json'}
+    webhook_url = config_info["bmc_credentials"]["bmc_ips"]
+    payload = {"msg_type": "text", "content": {"text": message}}
+    headers = {"Content-Type": "application/json"}
     response = requests.post(webhook_url, data=json.dumps(payload), headers=headers)
 
     if response.status_code != 200:
-        logger.info(f"Failed to send alert to Feishu status code: {response.status_code}")
+        logger.info(
+            f"Failed to send alert to Feishu status code: {response.status_code}"
+        )
     else:
         logger.error("Failed to send alert to Feishu")
